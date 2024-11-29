1e0ab8ebb24b59d4a1c7cc2e84d096302d530649
fares <fares.hamou0@gmail.com>
2024-11-29 14:56:02 +0100

    Initialisation de l'application avec firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';  // Pour l'authentification
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';  // Pour Firestore
+import { environment } from '../environments/environment';  // Assure-toi que tu importes tes infos Firebase
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),  // Initialisation de Firebase
+    AngularFireAuthModule,  // Pour l'authentification
+    AngularFirestoreModule  // Pour Firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD5g6bAsKKjeS18A8DqSBEXQb26UvXZohc",
+    authDomain: "locamat-13cca.firebaseapp.com",
+    projectId: "locamat-13cca",
+    storageBucket: "locamat-13cca.firebasestorage.app",
+    messagingSenderId: "1048772629549",
+    appId: "1:1048772629549:web:6eb1b3b3c0173f0ba994a4"
+  }
+};
