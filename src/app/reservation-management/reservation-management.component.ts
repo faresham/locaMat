64c7049b09c5b4ad83bf74073634fa3c67354ce1
Red <hitmanred59@gmail.com>
2025-01-23 09:49:10 +0700

    Ajout de la gestion des réservations et mise à jour du module principal

---
+++ src/app/reservation-management/reservation-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-management',
+  templateUrl: './reservation-management.component.html',
+  styleUrl: './reservation-management.component.css'
+})
+export class ReservationManagementComponent {
+
+}
